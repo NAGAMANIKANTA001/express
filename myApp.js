8672cb4ee0a058aa7928e4da788bdf8964b8cd3b
Naveen <Naveen.b@technovert.com>
2022-09-01 12:40:29 +0530

    Completed the module

--- myApp.js
+++ myApp.js
@@ -1,7 +1,10 @@
 require('dotenv').config()
 let express = require('express');
 let app = express();
+const bodyParser = require('body-parser');
 
+app.use(bodyParser.urlencoded({extended:false}))
+app.use(bodyParser.json());
 app.use((req,res,next)=>{
     console.log(`${req.method} ${req.path} - ${req.ip}`);
     next();
@@ -9,6 +12,23 @@ app.use((req,res,next)=>{
 
 app.use("/public",express.static(__dirname+"/public"))
 
+app.get("/now",(req,res,next)=>{
+    req.time = new Date().toString();
+    next();
+},(req,res)=>{
+    res.json({"time":req.time})
+})
+
+app.route("/name").get((req,res)=>{
+    res.json({name:`${req.query.first} ${req.query.last}`})
+}).post((req,res)=>{
+    console.log(req.body)
+    res.json({name:`${req.body.first} ${req.body.last}`})
+})
+
+app.get("/:word/echo",(req,res)=>{
+    res.json({echo:req.params.word})
+})
 
 app.get("/",function (req,res) {
     res.sendFile(__dirname+"/views/index.html")
